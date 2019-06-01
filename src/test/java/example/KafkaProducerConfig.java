a9ac1e29ee32db75be53d9e70aea60dcf0a591f5
hpardo <aresius@gmail.com>
2019-06-01 19:36:53 +0200

    Update gitIgnore Update pom Add minor refactors

--- src/test/java/example/KafkaProducerConfig.java
+++ src/test/java/example/KafkaProducerConfig.java
@@ -19,7 +19,7 @@ public class KafkaProducerConfig {
   private String bootstrapAddress;
 
   @Bean
-  public ProducerFactory<String, String> producerFactory() {
+  private ProducerFactory<String, String> producerFactory() {
     Map<String, Object> configProps = new HashMap<>();
     configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
     configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
